dfbf274805f02f500be126920bd3635eb0737f5c
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-27 01:11:06 +0530

    partially added match card function

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const cards = document.querySelectorAll(".card");
+let cardOne, cardTwo;
 
-console.log(cards);
+function flipCard(e) {
+  let clickedCard = e.target;
+  if (clickedCard !== cardOne) {
+    clickedCard.classList.add("flip");
+    if (!cardOne) {
+      return (cardOne = clickedCard);
+    }
+
+    cardTwo = clickedCard;
+    let cardOneImg = cardOne.querySelector("img");
+    cardTwoImg = cardTwo.querySelector("img");
+    matchCards(cardOneImg, cardTwoImg);
+  }
+}
+function matchCards(img1, img2) {
+  console.log(img1, img2);
+}
+
+cards.forEach((card) => {
+  // adding click event to all cards
+  card.addEventListener("click", flipCard);
+});
