8e0ee6e7d7f343ef03f0b3421b109adcd63b8e78
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-27 06:30:59 +0530

    added timeout and match card function and added styling

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 const cards = document.querySelectorAll(".card");
 let cardOne, cardTwo;
+let disableDeck = false;
 
 function flipCard(e) {
   let clickedCard = e.target;
-  if (clickedCard !== cardOne) {
+  if (clickedCard !== cardOne && !disableDeck) {
     clickedCard.classList.add("flip");
     if (!cardOne) {
       return (cardOne = clickedCard);
     }
 
     cardTwo = clickedCard;
-    let cardOneImg = cardOne.querySelector("img");
-    cardTwoImg = cardTwo.querySelector("img");
+    disableDeck = true;
+    let cardOneImg = cardOne.querySelector("img").src;
+    let cardTwoImg = cardTwo.querySelector("img").src;
     matchCards(cardOneImg, cardTwoImg);
   }
 }
+
 function matchCards(img1, img2) {
-  console.log(img1, img2);
+  if (img1 === img2) {
+    cardOne.removeEventListener("click", flipCard);
+    cardTwo.removeEventListener("click", flipCard);
+    cardOne = cardTwo = "";
+    return (disableDeck = false);
+  }
+  setTimeout(() => {
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
+  setTimeout(() => {
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = ""; // Reset cards after flipping back
+    disableDeck = false;
+  }, 1200);
 }
 
 cards.forEach((card) => {
